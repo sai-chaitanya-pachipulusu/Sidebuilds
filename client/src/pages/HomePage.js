e06719afa7cf6af3cb2b81027802f6f833ccedca
Sai Chaitanya Pachipulusu <chaitanya17.sai@gmail.com>
2025-05-10 03:16:29 -0400

    Update

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -11,15 +11,6 @@ const ArrowIcon = () => (
   </svg>
 );
 
-// Secure icon for security information
-const SecureIcon = () => (
-  <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <path d="M19 11H5V21H19V11Z" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-    <path d="M17 11V7C17 4.23858 14.7614 2 12 2C9.23858 2 7 4.23858 7 7V11" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-    <path d="M12 16.5C12.8284 16.5 13.5 15.8284 13.5 15C13.5 14.1716 12.8284 13.5 12 13.5C11.1716 13.5 10.5 14.1716 10.5 15C10.5 15.8284 11.1716 16.5 12 16.5Z" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-  </svg>
-);
-
 function HomePage() {
   const { isAuthenticated } = useAuth();
   
